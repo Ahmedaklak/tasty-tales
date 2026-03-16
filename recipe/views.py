49308dee07f1752ca4cc5cbb5e619c0772e5d82d
Ahmedaklak <a.aklak@yahoo.co.uk>
2026-03-16 02:24:14 +0000

    add all recipe views

--- recipe/views.py
+++ recipe/views.py
@@ -1,31 +1,83 @@
-from django.http import HttpResponse
 from django.views import generic
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import Recipe
-
-
-def home_page_view(_request):
-    return HttpResponse("Recipe Home Page")
+from .forms import RecipeForm
 
 
 class RecipeList(generic.ListView):
+    """Display all published recipes on the home page."""
+
     model = Recipe
     queryset = Recipe.objects.filter(status=1)
-    template_name = "recipes/recipe_list.html"
-    context_object_name = "recipes"
+    template_name = 'recipe/recipe_list.html'
+    context_object_name = 'recipes'
     paginate_by = 6
 
 
 class RecipeDetail(generic.DetailView):
+    """Display a single recipe's full details."""
+
     model = Recipe
-    template_name = "recipes/recipe_detail.html"
-    context_object_name = "recipe"
-    slug_field = "slug"
-    slug_url_kwarg = "slug"
+    template_name = 'recipe/recipe_detail.html'
+    context_object_name = 'recipe'
 
 
-def recipe_edit(_request, slug):
-    return HttpResponse(f"Edit page for {slug} (not implemented yet)")
+class RecipeCreate(LoginRequiredMixin, generic.CreateView):
+    """Allow logged-in users to create a new recipe."""
+
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipe/recipe_form.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(
+            self.request, 'Recipe created successfully!'
+        )
+        return super().form_valid(form)
 
 
-def recipe_delete(_request, slug):
-    return HttpResponse(f"Delete page for {slug} (not implemented yet)")
+class RecipeEdit(LoginRequiredMixin, UserPassesTestMixin,
+                 generic.UpdateView):
+    """Allow recipe authors to edit their own recipes."""
+
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipe/recipe_form.html'
+
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
+
+    def get_success_url(self):
+        return reverse_lazy(
+            'recipe_detail', kwargs={'slug': self.object.slug}
+        )
+
+    def form_valid(self, form):
+        messages.success(
+            self.request, 'Recipe updated successfully!'
+        )
+        return super().form_valid(form)
+
+
+class RecipeDelete(LoginRequiredMixin, UserPassesTestMixin,
+                   generic.DeleteView):
+    """Allow recipe authors to delete their own recipes."""
+
+    model = Recipe
+    template_name = 'recipe/recipe_confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.author
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(
+            self.request, 'Recipe deleted successfully!'
+        )
+        return super().delete(request, *args, **kwargs)
