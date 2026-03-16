24778c81dc5b129d1b12cde498fd68a6cd41ecd7
Ahmedaklak <a.aklak@yahoo.co.uk>
2026-03-16 02:41:43 +0000

    configure URL routing

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,16 +1,30 @@
 from django.urls import path
-from .views import (
-    home_page_view,
-    RecipeList,
-    RecipeDetail,
-    recipe_edit,
-    recipe_delete,
-)
+from . import views
 
 urlpatterns = [
-    path("", home_page_view, name="home"),
-    path("recipes/", RecipeList.as_view(), name="recipe_list"),
-    path("recipes/<slug:slug>/", RecipeDetail.as_view(), name="recipe_detail"),
-    path("recipes/<slug:slug>/edit/", recipe_edit, name="recipe_edit"),
-    path("recipes/<slug:slug>/delete/", recipe_delete, name="recipe_delete"),
+    path(
+        '',
+        views.RecipeList.as_view(),
+        name='home'
+    ),
+    path(
+        'recipe/<slug:slug>/',
+        views.RecipeDetail.as_view(),
+        name='recipe_detail'
+    ),
+    path(
+        'recipe/new/',
+        views.RecipeCreate.as_view(),
+        name='recipe_create'
+    ),
+    path(
+        'recipe/<slug:slug>/edit/',
+        views.RecipeEdit.as_view(),
+        name='recipe_edit'
+    ),
+    path(
+        'recipe/<slug:slug>/delete/',
+        views.RecipeDelete.as_view(),
+        name='recipe_delete'
+    ),
 ]
